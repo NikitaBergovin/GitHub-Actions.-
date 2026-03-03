00924749205b9a584eb073067da28cfdc1b28280
NikitaBergovin <nikitosber123@gmail.com>
2026-03-03 19:51:41 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def mul(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("Умножение")
+    print("5 * 3 =", mul(5, 3))
+    print("10 * (-2) =", mul(10, -2))
+    print("0 * 7 =", mul(0, 7))
